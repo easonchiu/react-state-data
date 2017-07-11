3c6cc591ea625aca88bb98283c51d990909f6aa1
Eason chiu <76670758@qq.com>
2017-07-11 15:26:37 +0800

    update

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-import reactStateData from './dist'
-export default reactStateData
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _dist = require('./dist');
+
+var _dist2 = _interopRequireDefault(_dist);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _dist2.default;
