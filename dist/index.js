654e081e0efeaa4819ac85facf7695bed0be000c
Eason chiu <76670758@qq.com>
2017-07-12 12:56:01 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+    value: true
 });
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
@@ -15,52 +15,52 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 var data = function data(Comp) {
-	return function (props) {
-		Object.assign(Comp.prototype, {
-			setData: function setData(res, watch) {
-				var _this = this;
+    return function (props) {
+        Object.assign(Comp.prototype, {
+            setData: function setData(res, watch) {
+                var _this = this;
 
-				// watch
-				var watchs = {};
-				if (typeof watch === 'function') {
-					watchs = watch();
-				}
+                // watch
+                var watchs = {};
+                if (typeof watch === 'function') {
+                    watchs = watch();
+                }
 
-				// state
-				if (!this.state) {
-					this.state = _extends({}, res);
-				} else {
-					Object.assign(this.state, res);
-				}
+                // state
+                if (!this.state) {
+                    this.state = _extends({}, res);
+                } else {
+                    Object.assign(this.state, res);
+                }
 
-				if (this.data) {
-					console.warn('data must be null');
-				}
+                if (this.data) {
+                    console.warn('data must be null');
+                }
 
-				this.data = {};
-				var th = this;
+                this.data = {};
+                var th = this;
 
-				Object.keys(res).forEach(function (res) {
-					Object.defineProperty(_this.data, res, {
-						set: function set(val) {
-							if (th.state[res] !== val) {
-								var ov = th.state[res];
-								th.setState(_defineProperty({}, res, val), function (e) {
-									if (watchs[res] && typeof watchs[res] === 'function') {
-										watchs[res].call(th, th.state[res], ov);
-									}
-								});
-							}
-						},
-						get: function get() {
-							return th.state[res];
-						}
-					});
-				});
-			}
-		});
-		return _react2.default.createElement(Comp, props);
-	};
+                Object.keys(res).forEach(function (res) {
+                    Object.defineProperty(_this.data, res, {
+                        set: function set(val) {
+                            if (th.state[res] !== val) {
+                                var ov = th.state[res];
+                                th.setState(_defineProperty({}, res, val), function (e) {
+                                    if (watchs[res] && typeof watchs[res] === 'function') {
+                                        watchs[res].call(th, th.state[res], ov);
+                                    }
+                                });
+                            }
+                        },
+                        get: function get() {
+                            return th.state[res];
+                        }
+                    });
+                });
+            }
+        });
+        return _react2.default.createElement(Comp, props);
+    };
 };
 
 exports.default = data;
--- src/index.js
+++ src/index.js
@@ -1,55 +1,55 @@
 import React from 'react'
 
 const data = Comp => props => {
-	Object.assign(Comp.prototype, {
-		setData(res, watch) {
+    Object.assign(Comp.prototype, {
+        setData(res, watch) {
 
-			// watch
-			let watchs = {}
-			if (typeof watch === 'function') {
-				watchs = watch()
-			}
+            // watch
+            let watchs = {}
+            if (typeof watch === 'function') {
+                watchs = watch()
+            }
 
-			// state
-			if (!this.state) {
-				this.state = {
-					...res
-				}
-			} else {
-				Object.assign(this.state, res)
-			}
-			
-			if (this.data) {
-				console.warn('data must be null')
-			}
+            // state
+            if (!this.state) {
+                this.state = {
+                    ...res
+                }
+            } else {
+                Object.assign(this.state, res)
+            }
+            
+            if (this.data) {
+                console.warn('data must be null')
+            }
 
-			this.data = {}
-			const th = this
+            this.data = {}
+            const th = this
 
-			Object.keys(res).forEach(res => {
-				Object.defineProperty(this.data, res, {
-					set(val) {
-						if (th.state[res] !== val) {
-							const ov = th.state[res]
-							th.setState({
-								[res]: val
-							}, e => {
-								if (watchs[res] && typeof watchs[res] === 'function') {
-									watchs[res].call(th, th.state[res], ov)
-								}
-							})
-						}
-					},
-					get() {
-						return th.state[res]
-					}
-				})
-			})
-			
-			
-		}
-	})
-	return <Comp {...props} />
+            Object.keys(res).forEach(res => {
+                Object.defineProperty(this.data, res, {
+                    set(val) {
+                        if (th.state[res] !== val) {
+                            const ov = th.state[res]
+                            th.setState({
+                                [res]: val
+                            }, e => {
+                                if (watchs[res] && typeof watchs[res] === 'function') {
+                                    watchs[res].call(th, th.state[res], ov)
+                                }
+                            })
+                        }
+                    },
+                    get() {
+                        return th.state[res]
+                    }
+                })
+            })
+            
+            
+        }
+    })
+    return <Comp {...props} />
 }
 
 export default data
