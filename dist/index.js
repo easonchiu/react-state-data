f087d5ae411bcf3d9f86c11a4a211083228ab20c
Eason chiu <76670758@qq.com>
2017-07-27 13:05:22 +0800

    fix bug

--- dist/index.js
+++ dist/index.js
@@ -16,49 +16,47 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 var data = function data(Comp) {
     return function (props) {
-        Object.assign(Comp.prototype, {
-            setData: function setData(res, watch) {
-                var _this = this;
+        Comp.prototype.setData = function(res, watch) {
+            var _this = this;
 
-                // watch
-                var watchs = {};
-                if (typeof watch === 'function') {
-                    watchs = watch();
-                }
-
-                // state
-                if (!this.state) {
-                    this.state = _extends({}, res);
-                } else {
-                    Object.assign(this.state, res);
-                }
-
-                if (this.data) {
-                    console.warn('data must be null');
-                }
-
-                this.data = {};
-                var th = this;
-
-                Object.keys(res).forEach(function (res) {
-                    Object.defineProperty(_this.data, res, {
-                        set: function set(val) {
-                            if (th.state[res] !== val) {
-                                var ov = th.state[res];
-                                th.setState(_defineProperty({}, res, val), function (e) {
-                                    if (watchs[res] && typeof watchs[res] === 'function') {
-                                        watchs[res].call(th, th.state[res], ov);
-                                    }
-                                });
-                            }
-                        },
-                        get: function get() {
-                            return th.state[res];
-                        }
-                    });
-                });
+            // watch
+            var watchs = {};
+            if (typeof watch === 'function') {
+                watchs = watch();
             }
-        });
+
+            // state
+            if (!this.state) {
+                this.state = _extends({}, res);
+            } else {
+                Object.assign(this.state, res);
+            }
+
+            if (this.data) {
+                console.warn('data must be null');
+            }
+
+            this.data = {};
+            var th = this;
+
+            Object.keys(res).forEach(function (res) {
+                Object.defineProperty(_this.data, res, {
+                    set: function set(val) {
+                        if (th.state[res] !== val) {
+                            var ov = th.state[res];
+                            th.setState(_defineProperty({}, res, val), function (e) {
+                                if (watchs[res] && typeof watchs[res] === 'function') {
+                                    watchs[res].call(th, th.state[res], ov);
+                                }
+                            });
+                        }
+                    },
+                    get: function get() {
+                        return th.state[res];
+                    }
+                });
+            });
+        }
         return _react2.default.createElement(Comp, props);
     };
 };
